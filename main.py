55c25c8192993b7a0a134467d4a4e82d5f66639e
nate-sys <nathandawit555@gmail.com>
2022-07-05 18:23:07 +0300

    helpers

--- main.py
+++ main.py
@@ -7,9 +7,11 @@ seconds = 0
 minutes = 0
 hours = 0
 
+
 def update(window):
     updateTime()
     updateScreen(window)
+
 
 def updateTime():
     global seconds, minutes, hours
@@ -21,6 +23,7 @@ def updateTime():
         minutes = 0
         hours += 1
 
+
 def updateScreen(window):
     global seconds
     window.clear()
@@ -28,7 +31,7 @@ def updateScreen(window):
 
     (maxY, maxX) = window.getmaxyx()
 
-    window.addstr(maxY//2 - 2 ,maxX//2 - 3, ':    :', curses.A_BLINK)
+    window.addstr(maxY//2 - 2 ,maxX//2 - 3, ':    :')
     
     window.addstr(maxY//2 - 2 ,maxX//2 - 6,str(hours).zfill(2), curses.A_DIM)
     window.addstr(maxY//2 - 2 ,maxX//2 - 1,str(minutes).zfill(2),)
@@ -37,12 +40,15 @@ def updateScreen(window):
     window.addstr(maxY//2,maxX//2 - 11,'|' + 20*' ' + '|', curses.A_BOLD)
     window.addstr(maxY//2,maxX//2 - 10,(seconds//3 + 1)*' ', curses.A_REVERSE)
     
+    window.addstr(maxY - 2,maxX//2 - 7,'q : quit, r: reset')
 
     window.refresh()
+
 
 def resetTimer():
     global seconds, minutes, hours
     seconds, minutes, hours = 0, 0, 0
+
 
 def main(window):
     global seconds, minutes, hours
@@ -57,4 +63,6 @@ def main(window):
             resetTimer()
         elif ch == ord('q'):
             break
+
+
 wrapper(main)
